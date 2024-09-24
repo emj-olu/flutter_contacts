4380c2360ce56024ca17d75172a4ebfa95327b65
EsinShadrach <esinshadrachrafe@gmail.com>
2024-09-24 11:53:01 +0100

    Init project

---
+++ example/lib/contacts_picker_page.dart
@@ -0,0 +1,49 @@
+import "package:contacts_service_example/main.dart";
+import "package:flutter/material.dart";
+
+import "package:contacts_service/contacts_service.dart";
+
+class ContactPickerPage extends StatefulWidget {
+  const ContactPickerPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactPickerPageState createState() => _ContactPickerPageState();
+}
+
+class _ContactPickerPageState extends State<ContactPickerPage> {
+  Contact _contact;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _pickContact() async {
+    try {
+      final Contact contact = await ContactsService.openDeviceContactPicker(
+          iOSLocalizedLabels: iOSLocalizedLabels);
+      setState(() {
+        _contact = contact;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Contacts Picker Example")),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: _pickContact,
+            child: const Text("Pick a contact"),
+          ),
+          Text("Contact selected: ${_contact.displayName}"),
+        ],
+      )),
+    );
+  }
+}
